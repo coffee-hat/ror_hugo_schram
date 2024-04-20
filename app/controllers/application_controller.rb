f03e43413efa372c520b63edfce4f5b1bb347853
coffee-hat <schramhugo77@gmail.com>
2024-04-20 03:23:20 +0200

    workaround for render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    #before_action :authenticate_user!
 
     private
 
     def authenticate_user!
-        unless user_signed_in? || request.path == new_user_session_path
+        unless user_signed_in? || request.path == new_user_session_path || request.path == new_user_registration_path
             redirect_to new_user_session_path, alert: "You need to sign in first."
         end
     end
